b5dffffec296a0991d03f9456446d7859098213a
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2023-11-25 19:21:10 +0530

    Type and datatype

---
+++ script2/injs.js
@@ -0,0 +1,23 @@
+document.title="type and datatype";
+var a=10;
+var b=20;
+var c="hello";
+
+console.log(a+b+20);
+console.log(typeof a,typeof b,typeof c);
+
+let b={
+    name:"monster"
+}
+console.log(b)
+b.slary=1000
+console.log(b)
+
+//datatype
+//Number
+//null
+//undefined
+//Boolean
+//String
+//Symbol
+//being
