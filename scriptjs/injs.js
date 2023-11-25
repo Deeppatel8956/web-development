88920add8fbc64697a4cfa2587c81ef803b463cc
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2023-11-25 19:17:30 +0530

    Pop box

---
+++ scriptjs/injs.js
@@ -0,0 +1,19 @@
+console.log("hello")
+console.log("i am coder")
+console.log("team leader")
+
+alert("please enter the no");
+
+let isTrue=Number.parseInt(prompt("enter the no"));
+if (isTrue%2==0) {
+    alert("true")
+} else {
+    alert("false")
+}
+//pop box use only browser
+//pop box list
+//1 alert
+//2 prompt
+//3 confirm
+
+
