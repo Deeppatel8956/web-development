e5a057b83c87fd8f9dcb93eb7ed12325aeece6cb
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2023-11-26 14:16:45 +0530

    Loop

---
+++ script4/injs.js
@@ -0,0 +1,27 @@
+document.title="loop"
+
+let b=10;
+for (let i = 0; i < 100; i++) {
+    console.log(a+i)
+    
+}
+let obj ={
+    name:"harry",
+    role:"developer",
+    company:"sky AI"
+}
+for (const key in obj) {
+
+        const element = object[key];
+        console.log(key,element)    
+    
+}
+let i=10;
+while (i<9) {
+    console.log(i);
+    i++
+}
+do {
+    console.log(i);
+    i++
+} while (i<9);
