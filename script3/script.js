5bda193cf473549f373e6da054fd4652779105d6
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2023-11-26 14:13:55 +0530

    Condition statement

---
+++ script3/script.js
@@ -0,0 +1,37 @@
+document.title="if else statement"
+
+let age=19;
+// let grace=2;
+// age+=grace
+// console.log(age)
+// // console.log(age+grace)
+// console.log(age-grace)
+// console.log(age*grace)
+// console.log(age/grace)
+// console.log(age**grace)
+// if (age==18) {
+//     console.log("you can drive")
+// } else {
+//     console.log("you can not drive")
+   
+// }
+// if (age!=18) {
+//     console.log("you can drive")
+// } else {
+//     console.log("you can not drive")
+   
+// }
+
+// if elseif
+if (age==18) {
+    console.log("you can drive")
+}
+else if (age==0) {
+   console.log("are you kiddiing ") 
+} else {
+    console.log("you can not drive")
+   
+}
+a=10;
+b=12;
+c=a>b?(a+b):(a-b);
