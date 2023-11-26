49bf2f7f87098d59e63cf59b3c8e84007727b261
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2023-11-26 13:56:23 +0530

    String in js

---
+++ script5/inds.js
@@ -0,0 +1,19 @@
+document.title="string"
+let a="Monstar"
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a.length);
+let b="shah";
+let c="Harshil";
+console.log(`My surname is ${b} and my name is ${c}`);
+console.log(b.toLowerCase());
+console.log(b.toUpperCase());
+console.log(b.charAt(0));
+console.log(b.slice(1,2));
+console.log(b.startsWith("sh"));
+console.log(b.endsWith("ah"));
+console.log(b.replace("s","23000"));
+console.log(b.concat(c,"priya"));
+console.log(b.indexOf("a"));
