e940b0dc60b970cac74f2085b893d4e6d56d176e
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2024-01-19 21:41:14 +0530

    Practice1

---
+++ Practiceset1.js
@@ -0,0 +1,18 @@
+let a=45;
+let b="Demo";
+console.log(a+b);
+
+// Q2
+console.log(typeof a+b)
+
+// Q3
+const c={
+    name:"tset",
+    no:1000
+}
+// c="hello"
+
+// Q4
+c["friend"]="Nishant";
+console.log(c)
+
