3651cf8311347c93f5e44f2c60365c7eee776a1a
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2024-01-20 19:51:54 +0530

    Practiceset2

---
+++ Practiceset2.js
@@ -0,0 +1,37 @@
+// let age=Number.parseInt(prompt("enter the age"))
+// if (age>10&&age<20) {
+//     console.log("you can drive")
+// } else {
+//     console.log("you cannot drive")
+// }
+
+// Q2
+// let no=Number.parseInt(prompt("enter the no"))
+// if(no%2==0&&no%3==0){
+//     console.log("divaded")
+// }else{
+//     console.log("not divaded")
+// }
+
+
+// Q3
+let no = Number.parseInt(prompt("enter the no"))
+if (no % 2 == 0 || no % 3 == 0) {
+    console.log("divaded")
+} else {
+    console.log("not divaded")
+}
+
+// Q4
+let b = "Apple"
+switch (b) {
+    case a:
+        console.log("b ids")
+        break;
+    case b:
+        console.log("b ids")
+        break;
+    default:
+        console.log("default")
+        break;
+}
