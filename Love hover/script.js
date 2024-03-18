14b63dc2531800515924064f5086283c86f0fc69
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2024-03-18 19:07:37 +0530

    Love hover

---
+++ Love hover/script.js
@@ -0,0 +1,9 @@
+let con=document.querySelector('#container')
+let love=document.querySelector('i')
+
+con.addEventListener('dblclick',function () {
+    love.style.transform="translate(-50%,-50%) scale(1)"
+    setTimeout(function(){
+        love.style.transform="translate(-50%,-50%) scale(0)"
+    },3000)
+})
